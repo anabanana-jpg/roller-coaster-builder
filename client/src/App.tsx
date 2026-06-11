1065acd427a32969b9382ef3b6a0cdd6c67ecca0
anabanana-jpg <anastasiamoon2010@gmail.com>
2026-06-11 09:54:28 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,7 @@ function MusicController() {
     dayMusic.volume = 0.5;
     setDaylightMusic(dayMusic);
     
-    const nightMusicAudio = new Audio(`${base}sounds/lovelyday.mp3`);
+    const nightMusicAudio = new Audio(`${base}sounds/daytimemusic.mp3`);
     nightMusicAudio.loop = true;
     nightMusicAudio.volume = 0.5;
     setNightMusic(nightMusicAudio);
